d2ca4da6ffa26798c48547a7f421224e631b1b7a
elbashandy <a.elbashandy@gmail.com>
2020-03-17 15:32:00 -0700

    Fixing ctest config file

--- travis_ci/ctest_linux.sh
+++ travis_ci/ctest_linux.sh
@@ -4,8 +4,8 @@ export DASHROOT=`pwd`
 export SHA=`git log --pretty=format:'%h' -n 1`
 if [[ "$DOCKER_IMAGE" == "fedora" ]]; then
     source /usr/share/Modules/init/bash
-    module load mpi
 fi
 mkdir build
+cd build
 cmake --version
 ctest -S ${DASHROOT}/travis_ci/ctest_linux.cmake -V --timeout 180
