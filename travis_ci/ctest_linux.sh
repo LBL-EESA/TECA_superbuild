4497c9bfafbfe30e8e9e2b204417fcb4b19f8b40
elbashandy <a.elbashandy@gmail.com>
2020-08-03 16:59:58 -0700

    squash: install with root but connect with non-root

--- travis_ci/ctest_linux.sh
+++ travis_ci/ctest_linux.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 set -v
 
-export TECA_BUILD_CORES=2
 if [[ "$DOCKER_IMAGE" == "fedora" ]]; then
     source /usr/share/Modules/init/bash
 fi
 mkdir build
 cd build
 
-export OMPI_ALLOW_RUN_AS_ROOT=1
-export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
-
 cmake --version
 cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ..
-make -j${TECA_BUILD_CORES} install
+make -j install
