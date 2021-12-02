ca2f1b3b1730751190099fb58e529dec51e51b8f
Burlen Loring <bloring@lbl.gov>
2021-12-02 13:07:32 -0800

    cori site install script install to a cori directory

--- site/make_install_cori_m1517.sh
+++ site/make_install_cori_m1517.sh
@@ -19,7 +19,7 @@ set -e
 
 # get the root of the install
 TECA_REV=`git ls-remote git@github.com:LBL-EESA/TECA.git | grep ${TECA_SOURCE} |  cut -c1-8`
-: ${TECA_PREFIX:=/global/common/software/m1517/teca}
+: ${TECA_PREFIX:=/global/common/software/m1517/teca/cori}
 : ${PREFIX:=${TECA_PREFIX}/${TECA_SOURCE}-${TECA_REV}}
 
 # mark as dependency only
